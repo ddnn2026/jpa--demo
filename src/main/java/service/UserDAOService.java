ec5a40f1ea7f32f59e3d76bbacda2ba1344ff652
David Nguyen <ddnn2026@gmail.com>
2023-12-06 07:29:44 +0700

    first

---
+++ src/main/java/service/UserDAOService.java
@@ -0,0 +1,11 @@
+package service;
+
+import entity.User;
+import jakarta.persistence.EntityManager;
+
+public class UserDAOService {
+    private EntityManager entityManager;
+    public long insert(User user){
+        return user;
+    }
+}
